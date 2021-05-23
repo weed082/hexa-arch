421c7b63ea398151581ead4215ab6387d2ba9525
ByungHakNoh <nbh6579@naver.com>
2021-05-23 10:52:06 +0900

    [UPDATE] router.go : 미들웨어 기능 추가

--- core/router.go
+++ core/router.go
@@ -8,15 +8,14 @@ import (
 )
 
 type key int
+type middleware func(http.Handler) http.Handler
 
-const (
-	paramKey key = iota
-)
-const anyMethod = "*"
+const paramKey key = iota
 
 type Router struct {
 	routes      map[string]map[string]http.HandlerFunc
 	regexRoutes map[string]map[*regexp.Regexp]http.HandlerFunc
+	middlewares []middleware
 }
 
 // ---------------- (1) Router 객체 생성 ----------------
@@ -44,43 +43,57 @@ func (router *Router) Delete(path string, handleFunc http.HandlerFunc) {
 	router.addRoute("DELETE", path, handleFunc)
 }
 
-func (router *Router) HandleFunc(path string, handleFunc http.HandlerFunc) {
-	router.addRoute(anyMethod, path, handleFunc)
-}
-
-// URL 파라미터가 필요한지에 따라 각각 regexRoutes, routes에 저장
 func (router *Router) addRoute(method, path string, handleFunc http.HandlerFunc) {
 	if strings.Contains(path, ":") {
 		_, ok := router.regexRoutes[method]
 		if !ok {
 			router.regexRoutes[method] = make(map[*regexp.Regexp]http.HandlerFunc)
 		}
-		regexPath := router.makeRegexPath(path)
-		router.regexRoutes[method][regexPath] = handleFunc
+		path = regexp.MustCompile(`:.[^/]+(?:\([^/]+\))`).ReplaceAllStringFunc(path, router.makeCustomRegexParam)
+		path = regexp.MustCompile(`(:[^/]+)`).ReplaceAllStringFunc(path, router.makeRegexParam)
+		regexPath := regexp.MustCompile("^" + path + "$")
+		chainedHandler := router.chain(handleFunc)
+		router.regexRoutes[method][regexPath] = chainedHandler.ServeHTTP
 		return
 	}
 	_, ok := router.routes[method]
 	if !ok {
 		router.routes[method] = make(map[string]http.HandlerFunc)
 	}
-	router.routes[method][path] = handleFunc
+	chainedHandler := router.chain(handleFunc)
+	router.routes[method][path] = chainedHandler.ServeHTTP
 }
 
-func (router *Router) makeRegexPath(path string) *regexp.Regexp {
-	customParamRegex := regexp.MustCompile(`:.[^(]+`)
-	path = regexp.MustCompile(`:.[^/]+(?:\([^/]+\))`).ReplaceAllStringFunc(path, func(s string) string {
-		s = customParamRegex.ReplaceAllStringFunc(s, func(s string) string {
-			return "(?P<" + s[1:] + ">"
-		})
-		return s + ")"
-	})
-	path = regexp.MustCompile(`(:[^/]+)`).ReplaceAllStringFunc(path, func(s string) string {
-		return "(?P<" + s[1:] + ">([^/]+))"
-	})
-	return regexp.MustCompile("^" + path + "$")
+func (router *Router) makeCustomRegexParam(param string) string {
+	splits := strings.SplitN(param, "(", 2)
+	return "(?P<" + splits[0][1:] + ">(" + splits[1] + ")"
 }
 
-// ---------------- (3) 요청 리스너 ----------------
+func (router *Router) makeRegexParam(param string) string {
+	return "(?P<" + param[1:] + ">([^/]+))"
+}
+
+// ---------------- (3) middleware ----------------
+func (router *Router) Use(middlewares ...middleware) {
+	if router.middlewares == nil {
+		router.middlewares = []middleware{}
+	}
+	router.middlewares = append(router.middlewares, middlewares...)
+}
+
+func (router *Router) chain(endpoint http.Handler) http.Handler {
+	if router.middlewares == nil {
+		return endpoint
+	}
+	handler := router.middlewares[0](endpoint)
+
+	for i := 1; i < len(router.middlewares); i++ {
+		handler = router.middlewares[i](handler)
+	}
+	return handler
+}
+
+// ---------------- (4) request listener ----------------
 func (router *Router) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	handleFunc, ok := router.routes[r.Method][r.URL.Path]
 	if ok {
@@ -102,12 +115,15 @@ func (router *Router) makeContextWithParams(r *http.Request, regexPath *regexp.R
 	params := make(map[string]string)
 
 	for i, paramKey := range regexPath.SubexpNames() {
-		params[paramKey] = paramValues[i]
+		if paramKey != "" {
+			params[paramKey] = paramValues[i]
+		}
 	}
 	context := context.WithValue(r.Context(), paramKey, params)
 	return r.WithContext(context)
 }
 
+// ---------------- (?) util ----------------
 func URLParam(r *http.Request) map[string]string {
 	if params := r.Context().Value(paramKey); params != nil {
 		return params.(map[string]string)
