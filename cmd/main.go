b9c7d39ef2cd2454d187b27763b20772d3377309
ByungHakNoh <nbh6579@naver.com>
2021-09-12 09:35:47 +0900

    [SORT] main.go : 코드 정렬

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,5 @@ package main
 import "github.com/ByungHakNoh/hexagonal-microservice/internal/framework/adapter/rest"
 
 func main() {
-	httpServer := rest.NewAdapter()
-	httpServer.Run()
+	rest.NewAdapter().Run()
 }
