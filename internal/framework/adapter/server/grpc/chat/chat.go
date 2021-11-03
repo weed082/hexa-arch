cef443a7b096424931db8ea23477fed6f88e12ff
ByungHakNoh <nbh6579@naver.com>
2021-11-03 20:56:05 +0900

    [UPDATE] chag.go : add sending method

--- internal/framework/adapter/server/grpc/chat/chat.go
+++ internal/framework/adapter/server/grpc/chat/chat.go
@@ -1,8 +1,27 @@
 package chat
 
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
 type Server struct {
 }
 
+func NewServer() *Server {
+	return &Server{}
+}
+
 func (s *Server) ChatService(csi ChatService_ChatServiceServer) error {
+	fmt.Println("client connected")
+	for {
+		err := csi.Send(&Message{Name: "name1", Body: "body1"})
+		if err != nil {
+			log.Fatalf("error sending message: %s", err)
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
 	return nil
 }
