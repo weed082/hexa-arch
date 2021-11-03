72def628ece7e34976a9236d66154abd93fc4e1f
ByungHakNoh <nbh6579@naver.com>
2021-11-03 22:20:19 +0900

    [UPDATE] chat client : add chat functionality

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/ByungHakNoh/hexagonal-microservice/internal/framework/adapter/server/grpc/chat"
 	"google.golang.org/grpc"
@@ -20,10 +22,29 @@ func main() {
 	if err != nil {
 		log.Fatalf("response error: %s", err)
 	}
+
+	go receiveMessage(stream)
+	sendMessage(stream)
+}
+
+func sendMessage(stream chat.ChatService_ChatServiceClient) {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		err := stream.Send(&chat.Message{Name: "client", Body: scanner.Text()})
+		if err != nil {
+			log.Fatalf("send to server err: %s", err)
+			break
+		}
+	}
+}
+
+func receiveMessage(stream chat.ChatService_ChatServiceClient) {
 	for {
 		message, err := stream.Recv()
 		if err != nil {
 			log.Fatalf("client stream error: %s", err)
+			break
 		}
 		fmt.Println(message.Name, message.Body)
 	}
