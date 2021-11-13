f312c4f2a988c88710b835dc1269b3baad947af9
ByungHakNoh <nbh6579@naver.com>
2021-11-13 17:16:44 +0900

    [UPDATE] grpc server update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"github.com/ByungHakNoh/hexagonal-microservice/internal/application"
+	"github.com/ByungHakNoh/hexagonal-microservice/internal/framework/adapter/repository"
+	"github.com/ByungHakNoh/hexagonal-microservice/internal/framework/adapter/repository/mysql"
 	"github.com/ByungHakNoh/hexagonal-microservice/internal/framework/adapter/server/grpc"
 )
 
+const (
+	dbaseDriver = "mysql"
+	dsourceName = "root:Admin123@/test"
+)
+
 func main() {
-	var testRepo struct{}
-	fileApp := application.NewFileApp(testRepo)
-	server := grpc.NewServer(fileApp)
+	chatSql := mysql.NewMysql(dbaseDriver, dsourceName)
+	chatRepo := repository.NewChatRepo(chatSql)
+	chatApp := application.NewChatApp(chatRepo)
+	server := grpc.NewServer(chatApp)
 	server.Run("9000")
 }
