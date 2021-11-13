60a78cc40b79f95f1938f3e5162ae1390947721b
ByungHakNoh <nbh6579@naver.com>
2021-11-14 01:22:31 +0900

    [FIX]

--- internal/framework/adapter/server/rest/core/router/util.go
+++ internal/framework/adapter/server/rest/core/router/util.go
@@ -4,8 +4,9 @@ import "net/http"
 
 // ---------------- (?) util ----------------
 func UrlParam(r *http.Request) map[string]string {
-	if params := r.Context().Value(paramKey); params != nil {
-		return params.(map[string]string)
+	params := r.Context().Value(paramKey)
+	if params == nil {
+		return nil
 	}
-	return nil
+	return params.(map[string]string)
 }
