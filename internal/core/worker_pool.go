cb1168f1a80f490fc5b76982cf933650472803ea
ByungHakNoh <nbh6579@naver.com>
2021-11-15 22:57:14 +0900

    [UPDATE] worker_pool : add

---
+++ internal/core/worker_pool.go
@@ -0,0 +1,67 @@
+package core
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Job struct {
+	Callback func()
+}
+
+type WorkerPool struct {
+	wg         *sync.WaitGroup
+	ctx        context.Context
+	jobChan    chan Job
+	ingestChan chan Job
+}
+
+func NewWorkerPool(wg *sync.WaitGroup, ctx context.Context, jobChan chan Job, ingestChan chan Job) *WorkerPool {
+	return &WorkerPool{
+		wg:         wg,
+		ctx:        ctx,
+		jobChan:    jobChan,
+		ingestChan: ingestChan,
+	}
+}
+
+//! (1) generate worker
+func (c WorkerPool) Generate(count int) {
+	c.wg.Add(count)
+	for i := 0; i < count; i++ {
+		go c.work()
+	}
+}
+
+func (c WorkerPool) work() {
+	defer c.wg.Done()
+	for job := range c.jobChan {
+		log.Println("worker started the work")
+		job.Callback()
+		log.Println("worker finished the work")
+	}
+	log.Println("worker destroyed")
+}
+
+//! (2) start worker pool
+func (c WorkerPool) Start() {
+	for {
+		select {
+		case job := <-c.ingestChan:
+			log.Println("job ingested")
+			c.jobChan <- job
+		case <-c.ctx.Done():
+			fmt.Println("Consumer received cancellation signal, closing jobChan")
+			close(c.jobChan)
+			fmt.Println("Consumer closed jobsChan")
+			return
+		}
+	}
+}
+
+//! (3) register event callback
+func (c WorkerPool) RegisterEventCallback(job Job) {
+	c.ingestChan <- job
+}
