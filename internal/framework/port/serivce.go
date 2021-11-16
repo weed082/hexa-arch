0594c96f31b024a4fc4845644c49c6e49d607d08
ByungHakNoh <nbh6579@naver.com>
2021-11-16 14:25:00 +0900

    [sort]

--- internal/framework/port/serivce.go
+++ internal/framework/port/serivce.go
@@ -1,15 +1,15 @@
 package port
 
 //! ChatApp
-type Client interface {
-	GetUserIdx() int
-}
-
 type ChatApp interface {
 	CreateRoom(client Client, rooms map[int][]Client) (int, error)
 	RemoveRoom(roomIdx int, rooms map[int][]Client) error
 	JoinRoom(client Client, clients []Client) error
 	ExitRoom(roomIdx, userIdx int, rooms map[int][]Client) error
+}
+
+type Client interface {
+	GetUserIdx() int
 }
 
 //!  User
