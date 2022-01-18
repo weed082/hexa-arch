8f0c3370dce4f803082beba05c5c5892835e915a
ByungHakNoh <nbh6579@naver.com>
2022-01-18 05:22:22 +0000

    [UPDATE] user.go : add test request for example

--- internal/framework/adapter/server/rest/handler/user.go
+++ internal/framework/adapter/server/rest/handler/user.go
@@ -23,16 +23,37 @@ func NewUser(logger *log.Logger, app port.User) *User {
 	}
 }
 
-func (handler *User) Register(r port.Router) {
-	r.Get("/test/:param", handler.test)
+func (h *User) Register(r port.Router) {
+	r.Get("/test/:param(param|sub)", h.test)
+	r.Post("/user/create", h.create)
 }
 
-func (handler *User) test(w http.ResponseWriter, r *http.Request) {
+func (h *User) test(w http.ResponseWriter, r *http.Request) {
 	urlParams := router.UrlParam(r)
-	user := model.User{Idx: 4, Name: urlParams["param"]}
+	user := model.User{Idx: 3, Name: urlParams["param"]}
 	jsonBytes, err := json.Marshal(user)
 	if err != nil {
-		handler.logger.Printf("json marshal failed : %s", err)
+		h.logger.Printf("json marshal failed : %s", err)
 	}
 	fmt.Fprintln(w, string(jsonBytes))
+}
+
+// testing the post req, res
+type SomeStruct struct {
+	Test string
+}
+
+func (h *User) create(w http.ResponseWriter, r *http.Request) {
+	var someStruct SomeStruct
+	err := json.NewDecoder(r.Body).Decode(&someStruct)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	res, err := json.Marshal(someStruct)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, string(res))
 }
