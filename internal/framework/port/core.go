3ba89c3095ea50a1b5b921fd1d0ed24a69f58e8a
ByungHakNoh <nbh6579@naver.com>
2022-02-10 22:50:49 +0900

    [DEL] port service : delete router

--- internal/framework/port/core.go
+++ internal/framework/port/core.go
@@ -1,18 +1,7 @@
 package port
 
-import "net/http"
-
 type WorkerPool interface {
 	Generate(count int)
 	Stop()
 	RegisterJob(callback func())
-}
-
-type Router interface {
-	Use(...func(http.Handler) http.Handler)
-	Get(path string, handleFunc http.HandlerFunc)
-	Post(path string, handleFunc http.HandlerFunc)
-	Put(path string, handleFunc http.HandlerFunc)
-	Delete(path string, handleFunc http.HandlerFunc)
-	ServeHTTP(rw http.ResponseWriter, r *http.Request)
 }
